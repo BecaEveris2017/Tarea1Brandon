c4cde6bff3b143bc4a79d1b83cbee49e6cc05de8
BrandonVasquez <spartanobjc@gmail.com>
2017-10-28 09:42:47 -0500

    Tarea 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  //title = 'app works!';
+
+  nombre2='Aplicacion: Brandon J. Castillo Vásquez';
+  Nombres: string[];
+  montrarNombres: boolean;
+
+  constructor(){
+this.Nombres=['Brandon','Jair'];
+this.montrarNombres=true;
+  }
+
+  ocuverNombres(){
+  this.montrarNombres=!this.montrarNombres;
+}
+nuevoNombre(newNombre){
+  this.Nombres.push(newNombre.value);
+  newNombre.value="";
+  return false;
+}
 }
